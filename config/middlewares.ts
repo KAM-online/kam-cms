414bfc5ded7f491d25dfa1ce8ef1b36ad28719c9
Bartosz Art <bartosz.art@gmail.com>
2025-10-22 13:55:39 +0200

    Revert: "FEAT: upload files to vercel blob"
    This reverts commit 3f339bffb5a79b21e26ee45f48f3c18144b0bd62.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,7 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -8,28 +9,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://k9v6cywaxb9ieton.public.blob.vercel-storage.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://k9v6cywaxb9ieton.public.blob.vercel-storage.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,1 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-vercel',
-      providerOptions: {
-        token: env('VERCEL_BLOB_TOKEN'),
-        addRandomSuffix: false,
-        cacheControlMaxAge: 31536000, // Year in seconds
-      },
-      breakpoints: null,
-    },
-  },
-});
+export default () => ({});
