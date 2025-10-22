3f339bffb5a79b21e26ee45f48f3c18144b0bd62
Bartosz Art <bartosz.art@gmail.com>
2025-10-22 13:16:09 +0200

    FEAT: upload files to vercel blob

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-vercel',
+      providerOptions: {
+        token: env('VERCEL_BLOB_TOKEN'),
+        addRandomSuffix: false,
+        cacheControlMaxAge: 31536000, // Year in seconds
+      },
+      breakpoints: null,
+    },
+  },
+});
