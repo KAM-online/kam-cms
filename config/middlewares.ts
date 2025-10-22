92e44036d6803574a99d4ef277431a059d4e3ffa
Bartosz Art <bartosz.art@gmail.com>
2025-10-22 13:57:43 +0200

    FEAT: #2 upload files to vercel blob

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,6 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,28 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://k9v6cywaxb9ieton.public.blob.vercel-storage.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://k9v6cywaxb9ieton.public.blob.vercel-storage.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@schornio/strapi-provider-upload-vercel-blob',
+      providerOptions: {
+        token: env('BLOB_READ_WRITE_TOKEN'),
+        addRandomSuffix: false,
+        cacheControlMaxAge: 31536000, // Year in seconds
+      },
+      breakpoints: null,
+    },
+  },
+});
